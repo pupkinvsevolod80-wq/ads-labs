cf245be6b5ca87e1ef81ca5d8547d5592aa964c1
pupkinvsevolod80-wq <pupkinvsevolod80@gmail.com>
2026-09-20 18:40:37 +0300

    Update H.py

--- Сортировки/H.py
+++ Сортировки/H.py
@@ -1,15 +1,27 @@
+from functools import cmp_to_key
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    def dist(self):
+        return self.x **2+ self.y **2
+
+
+def cmp_points(a, b):
+    return a.dist() - b.dist()
+
+
 n = int(input())
-points = []
+points = []                      
 
 for _ in range(n):
     x, y = map(int, input().split())
-    points.append(Point(x, y))
-points.sort(key=lambda p: p.x**2 + p.y**2)
+    points.append(Point(x, y))  
+
+points.sort(key=cmp_to_key(cmp_points))
 
 for p in points:
     print(p.x, p.y)
